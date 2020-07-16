508efe311b6131c13b0842a6d456c09968c9ce2b
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-16 19:10:14 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push(array, string)
+
+end
+def using_push(array, string)
+  array.push(string)
+end
+def using_push(array, string)
+  array.unshift(string)
+end
+def using_push(array)
+  element.pop
+end
+def using_push(array, 2)
+  2.pop
+end
+def using_push(array)
+  array.shift
+end
+def using_push(array, 2)
+  2.shift
+end
